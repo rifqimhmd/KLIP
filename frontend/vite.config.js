bc4f84136f77e4424274a2c03c2a23f9edfe572d
rifqimhmd <rifqimuh390@gmail.com>
2025-11-26 11:52:15 +0700

    update root

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "html-transform",
+      transformIndexHtml(html) {
+        return html.replace(
+          /<title>(.*?)<\/title>/,
+          `<title>Klinik Patnal – Konsultasi Aman, Respons Tepat</title>`
+        );
+      },
+    },
+  ],
+});
